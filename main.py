7056f681a08dc08eedc15673fb64e9d85b227d00
union198383-hue <union198383@gmail.com>
2026-03-18 00:32:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "🚀 Railway 연결 성공 (실서버 작동중)"
+})
+
+print("done")
